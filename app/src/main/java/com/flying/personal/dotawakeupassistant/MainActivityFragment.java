56fcd3c78b02622c85897072290f962f28103668
xiaoaowanghu <xiaoaowanghu@sina.com>
2015-08-12 17:03:27 +0800

    day3

--- app/src/main/java/com/flying/personal/dotawakeupassistant/MainActivityFragment.java
+++ app/src/main/java/com/flying/personal/dotawakeupassistant/MainActivityFragment.java
@@ -1,15 +1,15 @@
 package com.flying.personal.dotawakeupassistant;
 
-import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.Display;
+import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.flying.personal.dotawakeupassistant.model.Hero;
 
@@ -35,26 +35,26 @@ public class MainActivityFragment extends Fragment {
     private void LoadData(View fragmentRoot) {
         GridLayout gridLayout = (GridLayout) fragmentRoot.findViewById(R.id.gridLayout);
         List<Hero> heros = dataProvider.getAllHeros();
-        Display display = this.getActivity().getWindowManager().getDefaultDisplay(); //Activity#getWindowManager()
-        Point size = new Point();
-        display.getSize(size);
-        int screenWidth = size.x;
-        int picWidth = 72;
-        int colCount = screenWidth / (picWidth + 10);
-//        int rowCount = (int) Math.ceil(dataProvider.getTotalHeroCount() * 1.0 / colCount);
-//        GridLayout.LayoutParams params = (GridLayout.LayoutParams) gridLayout.getLayoutParams();
-//        params.columnSpec = GridLayout.spec(colCount);
-//        params.rowSpec = GridLayout.spec(rowCount);
+        DisplayMetrics dm = new DisplayMetrics();
+        this.getActivity().getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int screenWidthPX = dm.widthPixels;    //得到宽度
+        int colCount = 4;
+        int margin = 50;
+        int picWidthPX = (int) ((screenWidthPX - margin * colCount) / colCount * 1.0 - 0.5f);
 
         for (int i = 0; i < dataProvider.getTotalHeroCount(); i++) {
-            ImageView imageView =  new ImageView(this.getActivity());
-            imageView.setLayoutParams(new ViewGroup.LayoutParams(36, ViewGroup.LayoutParams.WRAP_CONTENT));
-            imageView.setImageResource(R.drawable.ic_launcher);
+            ImageView imageView = new ImageView(this.getActivity());
+            ViewGroup.LayoutParams img2Dimens = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            imageView.setLayoutParams(img2Dimens);
+            imageView.setMaxWidth(picWidthPX);
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imageView.setAdjustViewBounds(true);
 
             GridLayout.Spec rowSpec = GridLayout.spec(i / colCount);
             GridLayout.Spec columnSpec = GridLayout.spec(i % colCount);
             GridLayout.LayoutParams params = new GridLayout.LayoutParams(rowSpec, columnSpec);
             params.setGravity(Gravity.CENTER);
+            params.setMargins(margin, margin, 0, 0);
             gridLayout.addView(imageView, params);
         }
     }
